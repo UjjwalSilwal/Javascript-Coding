a87eba4c181b655b2cfa57cf92c464b716df8ec6
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2025-09-03 14:04:05 +0545

    javascript brocode complted

--- 4.accept_user_input/2/index.js
+++ 4.accept_user_input/2/index.js
@@ -1,7 +1,10 @@
 
 
+
 let username;
 
-username = window.prompt("What is your username?");
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`
 
-console.log(username);
+}
